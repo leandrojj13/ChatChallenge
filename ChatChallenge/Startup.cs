00f78d0336b1ae377bac1d33be60ff34edf177fd
Leandro Jimenez <leandro.jimenez@solvex.com.do>
2020-07-04 23:29:12 -0400

    Configurations of Cors, DbContext and JsonResult to CamelCase

--- ChatChallenge/Startup.cs
+++ ChatChallenge/Startup.cs
@@ -2,10 +2,12 @@
 using ChatChallenge.Core.IoC;
 using ChatChallenge.Core.IoC.Config;
 using ChatChallenge.IoC;
+using ChatChallenge.Model.Contexts.ChatChallenge;
 using ChatChallenge.Model.IoC;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -23,15 +25,48 @@ namespace ChatChallenge
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            #region CORS
+            services.AddCors(options =>
+            {
+                options.AddPolicy("AllowAllPolicy",
+                      builder =>
+                      {
+                          builder
+                                 .WithOrigins("http://localhost:8080")
+                                 .AllowAnyHeader()
+                                 .AllowAnyMethod()
+                                 .AllowCredentials()
+                                 ;
+                          builder.SetIsOriginAllowed(x => true);
+
+                      });
+            });
+            #endregion
 
             #region IoC Registry
             services.AddCoreRegistry();
             services.AddModelRegistry();
             services.AddBlRegistry();
             services.AddMainRegistry();
+            #endregion
+
+            #region ContextConfiguration
+            string myAppDbContextConnection = Configuration.GetConnectionString("DefaultConnection");
+            services.AddDbContext<ChatChallengeDbContext>(op => op.UseSqlServer(myAppDbContextConnection), ServiceLifetime.Transient);
+            #endregion
+
+            #region Global Api Config
+            services.AddMvc()
+               .AddJsonOptions(options => {
+                   options.SerializerSettings.ContractResolver =
+                       new Newtonsoft.Json.Serialization.CamelCasePropertyNamesContractResolver();
+               })
+                .SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
             Dependency.ServiceProvider = services.BuildServiceProvider();
             #endregion
+        
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -48,6 +83,9 @@ namespace ChatChallenge
             }
 
             app.UseHttpsRedirection();
+
+            app.UseCors("AllowAllPolicy");
+
             app.UseMvc();
         }
     }
